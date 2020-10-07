4c46607d83fc8141a4127f7474835d8d5c7f4b80
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2020-10-07 12:25:03 +0200

    Navbar added

--- the-boring-shop/src/App.js
+++ the-boring-shop/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
@@ -12,10 +13,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <ProductList />
-      <Detalis />
-      <Cart />
-      <Default />
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/detalis" component={Detalis}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
     </>
   );
 }
--- the-boring-shop/src/components/Default.js
+++ the-boring-shop/src/components/Default.js
@@ -4,7 +4,7 @@ export default class Default extends Component {
     render() {
         return (
             <div>
-                <h3>Default</h3>
+                <h3>Nie znaleziono strony sźć</h3>
             </div>
         )
     }
