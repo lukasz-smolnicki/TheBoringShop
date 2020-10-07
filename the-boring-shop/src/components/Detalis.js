e3b93f2bead5d7b397f0e565c61c882d4daf20b9
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2020-10-07 11:27:29 +0200

    App skeleton added

---
+++ the-boring-shop/src/components/Detalis.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Detalis extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from detalis</h3>
+            </div>
+        )
+    }
+}
