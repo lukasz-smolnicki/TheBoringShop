f26eae0856599c3af2a1da557c3cec5d186f2043
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2020-10-07 17:34:18 +0200

    Context API added

--- the-boring-shop/src/components/Product.js
+++ the-boring-shop/src/components/Product.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { ProductConsumer } from '../context'
 
 export default class Product extends Component {
     render() {
         return (
             <div>
-                <h3>Product</h3>
+                <h3>Productaaa</h3>
             </div>
         )
     }
